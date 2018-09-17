f91c187b372c2e53f5d5f623419bf507d0a5cd76
k5_yamayama <drugbox.yamate@gmail.com>
2018-09-17 16:43:33 +0900

    Add chapter 3

--- polls/views.py
+++ polls/views.py
@@ -6,11 +6,9 @@ from .models import Choice, Question
 
 
 def index(request):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, 'polls/detail.html', {'question': question})
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
